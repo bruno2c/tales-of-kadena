37ef7a21e1a3fe2dd981702ce5b3fc31db927b7d
Bruno Cardenas <brunoccdn@gmail.com>
2019-02-16 15:09:26 -0200

    Added background and enemy sprites

---
+++ src/Entity/Monster.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\MonsterRepository")
+ */
+class Monster
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $attack;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $defense;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $health;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $sprite_path;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAttack(): ?int
+    {
+        return $this->attack;
+    }
+
+    public function setAttack(?int $attack): self
+    {
+        $this->attack = $attack;
+
+        return $this;
+    }
+
+    public function getDefense(): ?int
+    {
+        return $this->defense;
+    }
+
+    public function setDefense(?int $defense): self
+    {
+        $this->defense = $defense;
+
+        return $this;
+    }
+
+    public function getHealth(): ?int
+    {
+        return $this->health;
+    }
+
+    public function setHealth(?int $health): self
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    public function getSpritePath(): ?string
+    {
+        return $this->sprite_path;
+    }
+
+    public function setSpritePath(?string $sprite_path): self
+    {
+        $this->sprite_path = $sprite_path;
+
+        return $this;
+    }
+}
