6105c1c48b9c49361adfee44253b0689b3afef72
Bruno Cardenas <brunoccdn@gmail.com>
2019-02-16 15:46:16 -0200

    Added some random monsters and health bar

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Monster;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -9,6 +10,12 @@ class IndexController extends AbstractController
 {
     public function index()
     {
-        return $this->render('index/index.html.twig');
+        $enemies = $this->getDoctrine()->getRepository(Monster::class)->findAll();
+        shuffle($enemies);
+        $enemies = array_slice($enemies, 0, 3);
+
+        return $this->render('index/index.html.twig', [
+            'enemies' => $enemies
+        ]);
     }
 }
